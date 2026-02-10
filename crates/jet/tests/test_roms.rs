df1e1fe10b2db0aacbc0199c4c777235dae081a2
Tyler Smith <mail@tcry.pt>
2026-02-10 17:48:00 -0600

    Revert "Reapply "Better rom building.""
    This reverts commit b2b63cd9a4232ac655a39b81b4d7902c3a1a62b4.

--- crates/jet/tests/test_roms.rs
+++ crates/jet/tests/test_roms.rs
@@ -4,78 +4,14 @@ use roms::*;
 
 mod roms;
 
-// Keep your flattening macro
-macro_rules! bytecode {
-    ($($item:expr),* $(,)?) => {
-        vec![$($item),*].into_iter().flatten().collect::<Vec<u8>>()
-    };
-}
-
-// 1. Meta-macro for simple opcodes (0 arguments)
-// This iterates over the provided names and generates a macro for each.
-macro_rules! define_ops {
-    ($($op:ident),* $(,)?) => {
-        $(
-            macro_rules! $op {
-                () => { vec![Instruction::$op.opcode()] };
-            }
-        )*
-    };
-}
-
-use paste::paste;
-
-macro_rules! generate_push_macros {
-    ($($n:literal),*) => {
-        paste! {
-            $(
-                // Generates macro_rules! PUSH1 { ... }
-                macro_rules! [<PUSH $n>] {
-                    ($($b:expr),+) => {{
-                        // References Instruction::PUSH1
-                        let mut v = vec![Instruction::[<PUSH $n>].opcode()];
-                        $(v.push($b);)+
-                        v
-                    }};
-                }
-            )*
-        }
-    };
-}
-
-// Generate PUSH1 through PUSH32
-generate_push_macros!(
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
-    27, 28, 29, 30, 31, 32
-);
-
-define_ops!(
-    ADD,
-    MUL,
-    SUB,
-    DIV,
-    MOD,
-    EXP,
-    SIGNEXTEND,
-    KECCAK256,
-    JUMP,
-    JUMPDEST,
-    PC,
-    MLOAD,
-    MSTORE,
-    MSTORE8,
-    RETURN,
-    CALL,
-    RETURNDATASIZE,
-    RETURNDATACOPY
-);
-
 rom_tests! {
     one_plus_two: Test {
-        roms: vec![bytecode![
-            PUSH1!(0x01),
-            PUSH1!(0x02),
-            ADD!(),
+        roms: vec![vec![
+            Instruction::PUSH1.opcode(),
+            0x01,
+            Instruction::PUSH1.opcode(),
+            0x02,
+            Instruction::ADD.opcode(),
         ]],
         expected: TestContractRun {
             stack_ptr: 1,
@@ -85,11 +21,13 @@ rom_tests! {
     },
 
     basic_jump: Test {
-        roms: vec![bytecode![
-            PUSH1!(0x03),
-            JUMP!(),
-            JUMPDEST!(),
-            PUSH1!(42),
+        roms: vec![vec![
+            Instruction::PUSH1.opcode(),
+            0x03,
+            Instruction::JUMP.opcode(),
+            Instruction::JUMPDEST.opcode(),
+            Instruction::PUSH1.opcode(),
+            42,
         ]],
         expected: TestContractRun {
             stack_ptr: 1,
@@ -100,17 +38,24 @@ rom_tests! {
     },
 
     basic_mem_ops: Test {
-        roms: vec![bytecode![
-            PUSH1!(0xFF),
-            PUSH1!(0x02),
-            MSTORE!(),
-            PUSH1!(0x00),
-            MLOAD!(),
-            PUSH2!(0xFF, 0xFF),
-            PUSH1!(0x00),
-            MSTORE8!(),
-            PUSH1!(0x00),
-            MLOAD!(),
+        roms: vec![vec![
+            Instruction::PUSH1.opcode(),
+            0xFF,
+            Instruction::PUSH1.opcode(),
+            0x02,
+            Instruction::MSTORE.opcode(),
+            Instruction::PUSH1.opcode(),
+            0x00,
+            Instruction::MLOAD.opcode(),
+            Instruction::PUSH2.opcode(),
+            0xFF,
+            0xFF,
+            Instruction::PUSH1.opcode(),
+            0x00,
+            Instruction::MSTORE8.opcode(),
+            Instruction::PUSH1.opcode(),
+            0x00,
+            Instruction::MLOAD.opcode(),
         ]],
         expected: TestContractRun {
             stack_ptr: 2,
@@ -120,14 +65,18 @@ rom_tests! {
     },
 
     vstack_accesses_real_stack_after_jump: Test{
-        roms: vec![bytecode![
-            PUSH1!(0x01),
-            PUSH1!(0x02),
-            PUSH1!(0x07),
-            JUMP!(),
-            JUMPDEST!(),
-            ADD!(),
-            PUSH1!(42),
+        roms: vec![vec![
+            Instruction::PUSH1.opcode(),
+            0x01,
+            Instruction::PUSH1.opcode(),
+            0x02,
+            Instruction::PUSH1.opcode(),
+            0x07,
+            Instruction::JUMP.opcode(),
+            Instruction::JUMPDEST.opcode(),
+            Instruction::ADD.opcode(),
+            Instruction::PUSH1.opcode(),
+            42,
         ]],
         expected: TestContractRun {
             stack_ptr: 2,
@@ -138,10 +87,12 @@ rom_tests! {
     },
 
     return_sets_offset_and_length: Test{
-        roms: vec![bytecode![
-            PUSH1!(0x20),
-            PUSH1!(0x03),
-            RETURN!(),
+        roms: vec![vec![
+            Instruction::PUSH1.opcode(),
+            0x20,
+            Instruction::PUSH1.opcode(),
+            0x03,
+            Instruction::RETURN.opcode(),
         ]],
         expected: TestContractRun {
             result: ReturnCode::ExplicitReturn,
@@ -152,30 +103,47 @@ rom_tests! {
     },
 
     basic_call_with_return_data: Test {
-        roms: vec![bytecode![
-            PUSH1!(0x0A), // Output len
-            PUSH1!(0x00), // Output offset
-            PUSH1!(0x00), // Input len
-            PUSH1!(0x00), // Input offset
-            PUSH1!(0x00), // Value
-            PUSH2!(0x00, 0x01), // Address
-            PUSH1!(0x00), // Gas
-            CALL!(), // Mem: 0x00FF
-            RETURNDATASIZE!(),
-            PUSH1!(0x02), // Len
-            PUSH1!(0x00), // Src offset
-            PUSH1!(0x02), // Dest offset
-            RETURNDATACOPY!(), // Mem: 0x00FF00FF0000000000000000
-        ], bytecode![
-            PUSH1!(0xFF),
-            PUSH1!(0x01),
-            MSTORE!(), // Mem: 0x00FF
-            PUSH1!(0xFF),
-            PUSH1!(0x0A),
-            MSTORE!(), // Mem: 0x00FF0000000000000000FF
-            PUSH1!(0x0A),
-            PUSH1!(0x00),
-            RETURN!(), // Return 0x00FF0000000000000000
+        roms: vec![vec![
+            Instruction::PUSH1.opcode(), // Output len
+            0x0A,
+            Instruction::PUSH1.opcode(), // Output offset
+            0x00,
+            Instruction::PUSH1.opcode(), // Input len
+            0x00,
+            Instruction::PUSH1.opcode(), // Input offset
+            0x00,
+            Instruction::PUSH1.opcode(), // Value
+            0x00,
+            Instruction::PUSH2.opcode(), // Address
+            0x00,
+            0x01,
+            Instruction::PUSH1.opcode(), // Gas
+            0x00,
+            Instruction::CALL.opcode(), // Mem: 0x00FF
+            Instruction::RETURNDATASIZE.opcode(),
+            Instruction::PUSH1.opcode(), // Len
+            0x02,
+            Instruction::PUSH1.opcode(), // Src offset
+            0x00,
+            Instruction::PUSH1.opcode(), // Dest offset
+            0x02,
+            Instruction::RETURNDATACOPY.opcode(), // Mem: 0x00FF00FF0000000000000000
+        ], vec![
+            Instruction::PUSH1.opcode(),
+            0xFF,
+            Instruction::PUSH1.opcode(),
+            0x01,
+            Instruction::MSTORE.opcode(), // Mem: 0x00FF
+            Instruction::PUSH1.opcode(),
+            0xFF,
+            Instruction::PUSH1.opcode(),
+            0x0A,
+            Instruction::MSTORE.opcode(), // Mem: 0x00FF0000000000000000FF
+            Instruction::PUSH1.opcode(),
+            0x0A,
+            Instruction::PUSH1.opcode(),
+            0x00,
+            Instruction::RETURN.opcode(), // Return 0x00FF0000000000000000
         ]],
         expected: TestContractRun {
             stack_ptr: 2,
@@ -189,9 +157,9 @@ rom_tests! {
     },
 
     keccak256_empty_hash: Test {
-        roms: vec![bytecode![
-            PUSH0!(),
-            KECCAK256!(),
+        roms: vec![vec![
+            Instruction::PUSH0.opcode(),
+            Instruction::KECCAK256.opcode(),
         ]],
         expected: TestContractRun {
             stack_ptr: 1,
@@ -201,10 +169,10 @@ rom_tests! {
     },
 
     exp_two_cubed: Test {
-        roms: vec![bytecode![
-            PUSH1!(0x03), // exponent
-            PUSH1!(0x02), // base
-            EXP!(),
+        roms: vec![vec![
+            Instruction::PUSH1.opcode(), 0x03, // exponent
+            Instruction::PUSH1.opcode(), 0x02, // base
+            Instruction::EXP.opcode(),
         ]],
         expected: TestContractRun {
             stack_ptr: 1,
@@ -216,10 +184,10 @@ rom_tests! {
     // Endianness-sensitive: base=256 is stored LE as [0x00, 0x01, ...].
     // A BE bug would misread it as 1, giving 1^2=1 instead of 65536.
     exp_base_256_squared: Test {
-        roms: vec![bytecode![
-            PUSH1!(0x02),        // exponent
-            PUSH2!(0x01, 0x00),  // base = 256
-            EXP!(),
+        roms: vec![vec![
+            Instruction::PUSH1.opcode(), 0x02,        // exponent
+            Instruction::PUSH2.opcode(), 0x01, 0x00,  // base = 256
+            Instruction::EXP.opcode(),
         ]],
         expected: TestContractRun {
             stack_ptr: 1,
@@ -229,10 +197,10 @@ rom_tests! {
     },
 
     exp_zero_exponent: Test {
-        roms: vec![bytecode![
-            PUSH1!(0x00), // exponent = 0
-            PUSH1!(0x05), // base
-            EXP!(),
+        roms: vec![vec![
+            Instruction::PUSH1.opcode(), 0x00, // exponent = 0
+            Instruction::PUSH1.opcode(), 0x05, // base
+            Instruction::EXP.opcode(),
         ]],
         expected: TestContractRun {
             stack_ptr: 1,
@@ -242,10 +210,10 @@ rom_tests! {
     },
 
     signextend_sign_bit_clear: Test {
-        roms: vec![bytecode![
-            PUSH1!(0x7F), // x = 127
-            PUSH1!(0x00), // b = 0
-            SIGNEXTEND!(),
+        roms: vec![vec![
+            Instruction::PUSH1.opcode(), 0x7F, // x = 127
+            Instruction::PUSH1.opcode(), 0x00, // b = 0
+            Instruction::SIGNEXTEND.opcode(),
         ]],
         expected: TestContractRun {
             stack_ptr: 1,
@@ -255,10 +223,10 @@ rom_tests! {
     },
 
     signextend_sign_bit_set: Test {
-        roms: vec![bytecode![
-            PUSH1!(0x80), // x = 128
-            PUSH1!(0x00), // b = 0
-            SIGNEXTEND!(),
+        roms: vec![vec![
+            Instruction::PUSH1.opcode(), 0x80, // x = 128
+            Instruction::PUSH1.opcode(), 0x00, // b = 0
+            Instruction::SIGNEXTEND.opcode(),
         ]],
         expected: TestContractRun {
             stack_ptr: 1,
@@ -275,10 +243,10 @@ rom_tests! {
     // A BE bug would put the bytes reversed, so byte 1 = 0x00 and no extension
     // would occur, giving [0x80, 0x00, ...] instead of [0x00, 0x80, 0xFF, ...].
     signextend_multi_byte: Test {
-        roms: vec![bytecode![
-            PUSH2!(0x80, 0x00), // x = 0x8000
-            PUSH1!(0x01),       // b = 1
-            SIGNEXTEND!(),
+        roms: vec![vec![
+            Instruction::PUSH2.opcode(), 0x80, 0x00, // x = 0x8000
+            Instruction::PUSH1.opcode(), 0x01,       // b = 1
+            Instruction::SIGNEXTEND.opcode(),
         ]],
         expected: TestContractRun {
             stack_ptr: 1,
@@ -293,10 +261,10 @@ rom_tests! {
     },
 
     signextend_large_b_noop: Test {
-        roms: vec![bytecode![
-            PUSH1!(0xFF), // x = 255
-            PUSH1!(0x20), // b = 32 (>= 32, identity)
-            SIGNEXTEND!(),
+        roms: vec![vec![
+            Instruction::PUSH1.opcode(), 0xFF, // x = 255
+            Instruction::PUSH1.opcode(), 0x20, // b = 32 (>= 32, identity)
+            Instruction::SIGNEXTEND.opcode(),
         ]],
         expected: TestContractRun {
             stack_ptr: 1,
@@ -306,14 +274,15 @@ rom_tests! {
     },
 
     program_counter: Test {
-        roms: vec![bytecode![
-            PC!(),
-            PC!(),
-            PC!(),
-            PUSH1!(0x06),
-            JUMP!(),
-            JUMPDEST!(),
-            PC!(),
+        roms: vec![vec![
+            Instruction::PC.opcode(),
+            Instruction::PC.opcode(),
+            Instruction::PC.opcode(),
+            Instruction::PUSH1.opcode(),
+            0x06,
+            Instruction::JUMP.opcode(),
+            Instruction::JUMPDEST.opcode(),
+            Instruction::PC.opcode(),
         ]],
         expected: TestContractRun {
             stack_ptr: 4,
@@ -325,10 +294,10 @@ rom_tests! {
 
     // Memory expansion tests
     mstore_at_zero_expands_to_32: Test {
-        roms: vec![bytecode![
-            PUSH1!(0x42), // value
-            PUSH1!(0x00), // offset = 0
-            MSTORE!(),      // MSTORE writes 32 bytes
+        roms: vec![vec![
+            Instruction::PUSH1.opcode(), 0x42, // value
+            Instruction::PUSH1.opcode(), 0x00, // offset = 0
+            Instruction::MSTORE.opcode(),      // MSTORE writes 32 bytes
         ]],
         expected: TestContractRun {
             stack_ptr: 0,
@@ -338,10 +307,10 @@ rom_tests! {
     },
 
     mstore_at_31_expands_to_64: Test {
-        roms: vec![bytecode![
-            PUSH1!(0x42), // value
-            PUSH1!(0x1F), // offset = 31
-            MSTORE!(),      // MSTORE writes 32 bytes
+        roms: vec![vec![
+            Instruction::PUSH1.opcode(), 0x42, // value
+            Instruction::PUSH1.opcode(), 0x1F, // offset = 31
+            Instruction::MSTORE.opcode(),      // MSTORE writes 32 bytes
         ]],
         expected: TestContractRun {
             stack_ptr: 0,
@@ -351,10 +320,10 @@ rom_tests! {
     },
 
     mstore_at_32_expands_to_64: Test {
-        roms: vec![bytecode![
-            PUSH1!(0x42), // value
-            PUSH1!(0x20), // offset = 32
-            MSTORE!(),      // MSTORE writes 32 bytes
+        roms: vec![vec![
+            Instruction::PUSH1.opcode(), 0x42, // value
+            Instruction::PUSH1.opcode(), 0x20, // offset = 32
+            Instruction::MSTORE.opcode(),      // MSTORE writes 32 bytes
         ]],
         expected: TestContractRun {
             stack_ptr: 0,
@@ -364,10 +333,10 @@ rom_tests! {
     },
 
     mstore8_at_zero_expands_to_32: Test {
-        roms: vec![bytecode![
-            PUSH1!(0x42), // value
-            PUSH1!(0x00), // offset = 0
-            MSTORE8!(),     // MSTORE8 writes 1 byte
+        roms: vec![vec![
+            Instruction::PUSH1.opcode(), 0x42, // value
+            Instruction::PUSH1.opcode(), 0x00, // offset = 0
+            Instruction::MSTORE8.opcode(),     // MSTORE8 writes 1 byte
         ]],
         expected: TestContractRun {
             stack_ptr: 0,
@@ -377,10 +346,10 @@ rom_tests! {
     },
 
     mstore8_at_31_expands_to_32: Test {
-        roms: vec![bytecode![
-            PUSH1!(0x42), // value
-            PUSH1!(0x1F), // offset = 31
-            MSTORE8!(),     // MSTORE8 writes 1 byte
+        roms: vec![vec![
+            Instruction::PUSH1.opcode(), 0x42, // value
+            Instruction::PUSH1.opcode(), 0x1F, // offset = 31
+            Instruction::MSTORE8.opcode(),     // MSTORE8 writes 1 byte
         ]],
         expected: TestContractRun {
             stack_ptr: 0,
@@ -390,10 +359,10 @@ rom_tests! {
     },
 
     mstore8_at_32_expands_to_64: Test {
-        roms: vec![bytecode![
-            PUSH1!(0x42), // value
-            PUSH1!(0x20), // offset = 32
-            MSTORE8!(),     // MSTORE8 writes 1 byte
+        roms: vec![vec![
+            Instruction::PUSH1.opcode(), 0x42, // value
+            Instruction::PUSH1.opcode(), 0x20, // offset = 32
+            Instruction::MSTORE8.opcode(),     // MSTORE8 writes 1 byte
         ]],
         expected: TestContractRun {
             stack_ptr: 0,
@@ -403,19 +372,19 @@ rom_tests! {
     },
 
     memory_expansion_is_monotonic: Test {
-        roms: vec![bytecode![
+        roms: vec![vec![
             // First MSTORE expands to 32
-            PUSH1!(0x11),
-            PUSH1!(0x00),
-            MSTORE!(),
+            Instruction::PUSH1.opcode(), 0x11,
+            Instruction::PUSH1.opcode(), 0x00,
+            Instruction::MSTORE.opcode(),
             // Second MSTORE at smaller offset doesn't shrink memory
-            PUSH1!(0x22),
-            PUSH1!(0x00),
-            MSTORE!(),
+            Instruction::PUSH1.opcode(), 0x22,
+            Instruction::PUSH1.opcode(), 0x00,
+            Instruction::MSTORE.opcode(),
             // Third MSTORE at higher offset expands to 64
-            PUSH1!(0x33),
-            PUSH1!(0x20),
-            MSTORE!(),
+            Instruction::PUSH1.opcode(), 0x33,
+            Instruction::PUSH1.opcode(), 0x20,
+            Instruction::MSTORE.opcode(),
         ]],
         expected: TestContractRun {
             stack_ptr: 0,
